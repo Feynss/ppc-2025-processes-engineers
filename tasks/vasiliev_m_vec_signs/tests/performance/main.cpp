13f6b2f666154a89cb945a366d1b119ed60bd6e6
Feynss <michavasnn@gmail.com>
2025-10-30 12:31:55 +0300

    Fixed tests name

--- tasks/vasiliev_m_vec_signs/tests/performance/main.cpp
+++ tasks/vasiliev_m_vec_signs/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace vasiliev_m_vec_signs {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class VasilievMVecSignsPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(VasilievMVecSignsPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = VasilievMVecSignsPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, VasilievMVecSignsPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace vasiliev_m_vec_signs
